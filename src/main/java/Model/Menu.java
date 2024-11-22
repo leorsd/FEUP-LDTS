cb74f5aa4f0036c7f568700c09acce680148bb08
Lourenço Rodrigues <up202306331@up.pt>
2024-11-22 13:00:19 +0000

    feat: Scene interface and Menu class

---
+++ src/main/java/Model/Menu.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.util.List;
+
+public class Menu implements Scene{
+    private List<String> entries;
+    private int highlightedEntryIndex = 0;
+
+    public Menu(List<String> entries) {
+        this.entries = entries;
+    }
+
+    public void selectNextEntry() {
+        highlightedEntryIndex = (highlightedEntryIndex + 1) % entries.size();
+    }
+
+    public void selectPreviousEntry() {
+        highlightedEntryIndex = (highlightedEntryIndex - 1) % entries.size();
+    }
+
+    public String getHighlightedEntry() {
+        return entries.get(highlightedEntryIndex);
+    }
+}
