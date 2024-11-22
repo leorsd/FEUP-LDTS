722e88ce82920d5f440d870217680c190e302917
Lourenço Rodrigues <up202306331@up.pt>
2024-11-22 13:03:20 +0000

    feat: class Trap

---
+++ src/main/java/Model/Trap.java
@@ -0,0 +1,38 @@
+package Model;
+
+import java.awt.image.BufferedImage;
+
+public class Trap extends Element{
+    private String target;
+    private String color;
+
+    Trap(String target, String color, Position position) {
+        super(position, null);
+        this.target = target;
+        this.color = color;
+    }
+
+    Trap(String target, Position position, BufferedImage image) {
+        super(position, image);
+        this.target = target;
+        this.color = null;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color!=null) {
+            this.color = color;
+        }
+    }
+}
