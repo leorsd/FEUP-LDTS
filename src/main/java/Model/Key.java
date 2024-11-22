afa66978b51a9dd60751ffc37425405c9195940a
Lourenço Rodrigues <up202306331@up.pt>
2024-11-22 13:03:35 +0000

    feat: class Key

---
+++ src/main/java/Model/Key.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.awt.image.BufferedImage;
+
+public class Key extends Element{
+    Key(Position position, BufferedImage image) {
+        super(position, image);
+    }
+}
