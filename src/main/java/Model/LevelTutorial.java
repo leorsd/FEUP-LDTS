20b3675010931e20402f388bad0653a0e341f52b
Lourenço Rodrigues <up202306331@up.pt>
2024-11-22 13:05:26 +0000

    feat: added Level Tutorial class

---
+++ src/main/java/Model/LevelTutorial.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class LevelTutorial {
+    private String tutorial;
+
+    public LevelTutorial(String tutorial) {
+        this.tutorial = tutorial;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public void setTutorial(String tutorial) {
+        this.tutorial = tutorial;
+    }
+}
