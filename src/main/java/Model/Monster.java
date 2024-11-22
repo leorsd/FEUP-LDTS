583987b4a9bdc5c1220178ba8a0177a8b57efd92
Lourenço Rodrigues <up202306331@up.pt>
2024-11-22 13:04:33 +0000

    feat: Monster class

---
+++ src/main/java/Model/Monster.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.awt.image.BufferedImage;
+
+public class Monster extends Character{
+    private int health;
+
+    public Monster(int health, int sizeX, int sizeY, Position position, BufferedImage image) {
+        super(sizeX, sizeY, position, image);
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
