44a7d15f01f2fd1545a9d5e1d92b9d4c1fb19f8e
Leandro Resende <resendeleandro565@gmail.com>
2024-11-23 21:24:28 +0000

    feat: GUI interface

---
+++ src/main/java/GUI/GUI.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import Model.Elements.Characters.Monster;
+import Model.Elements.Characters.Player;
+import Model.Elements.Key;
+import Model.Elements.Trap;
+import Model.Elements.Wall;
+import Model.Position;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashSet;
+
+public interface GUI {
+    HashSet<ACTION> getNextAction() throws IOException;
+
+    void drawPlayer(Player player);
+
+    void drawWall(Wall wall);
+
+    void drawMonster(Monster monster);
+
+    void drawKey(Key key);
+
+    void drawTrap(Trap trap);
+
+    void drawText(Position position, BufferedImage image);
+
+    void clear();
+
+    void refresh() throws IOException;
+
+    void close() throws IOException;
+
+    enum ACTION {UP, DOWN, LEFT, RIGHT, W, D, S, A, QUIT, SELECT}
+}
