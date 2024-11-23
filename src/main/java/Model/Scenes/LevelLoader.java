c915968181ff309814f8abae90e3759bbaa43d56
Lourenço Rodrigues <up202306331@up.pt>
2024-11-23 22:37:00 +0000

    fix: added placeholder classes necessary to implement controller

---
+++ src/main/java/Model/Scenes/LevelLoader.java
@@ -0,0 +1,9 @@
+package Model.Scenes;
+
+public class LevelLoader {
+    public Level loadLevel(String levelName) {
+        return new Level(null, null, null , null, null, 0, 0);
+    }
+
+    //TODO: implement everything
+}
