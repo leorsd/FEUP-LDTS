8e251ef63722ebe75b4f7c6f35041fd784e8cca9
Leandro Resende <resendeleandro565@gmail.com>
2024-11-27 09:40:35 +0000

    fix: add cooldown in the menu controller

--- src/main/java/controller/menu/MenuController.java
+++ src/main/java/controller/menu/MenuController.java
@@ -10,28 +10,37 @@ import java.io.IOException;
 import java.util.Set;
 
 public class MenuController extends Controller<Menu> {
+    private long lastUpdateTime;
     public MenuController(Menu menu) {
         super(menu);
+        this.lastUpdateTime = 0;
     }
 
     @Override
     public void update(GameManager gameManager, Set<GUI.ACTION> actions, long updateTime) throws IOException {
-        if (actions.size() != 1) return;
+        if (!(actions.size() == 1)) return;
+
         GUI.ACTION action = actions.iterator().next();
-        switch (action) {
-            case UP:
-                getModel().selectPreviousEntry();
-                break;
-            case DOWN:
-                getModel().selectNextEntry();
-                break;
-            case SELECT:
-                if (getModel().getHighlightedEntry().equals("EXIT")) {
-                    gameManager.setCurrentScene(null);
-                } else {
-                    String level = getModel().getHighlightedEntry();
-                    gameManager.setCurrentScene((new LevelLoader()).loadLevel(level));
+
+        if(action == GUI.ACTION.SELECT){
+            if (getModel().getHighlightedEntry().equals("EXIT")) {
+                gameManager.setCurrentScene(null);
+            } else {
+                String level = getModel().getHighlightedEntry();
+                gameManager.setCurrentScene((new LevelLoader()).loadLevel(level));
+            }
+        }else {
+            if (updateTime - lastUpdateTime > 200) {
+                switch (action) {
+                    case UP:
+                        getModel().selectPreviousEntry();
+                        break;
+                    case DOWN:
+                        getModel().selectNextEntry();
+                        break;
                 }
+                lastUpdateTime = updateTime;
+            }
         }
     }
 }
