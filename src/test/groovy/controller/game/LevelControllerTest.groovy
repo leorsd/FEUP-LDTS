40e87b8edb52ec3d111306cc910f122012c7d771
eitexd <goncalomfsantos@gmail.com>
2024-12-12 22:13:36 +0000

    fix: fixed tests related to player deaths

--- src/test/groovy/controller/game/LevelControllerTest.groovy
+++ src/test/groovy/controller/game/LevelControllerTest.groovy
@@ -16,14 +16,12 @@ class LevelControllerTest extends Specification {
     def levelController = new LevelController(level)
     def gameManager = Mock(GameManager)
 
-    def "checkDeadPlayers should return true if a player collides with a monster"() {
-        given: "a game with players and a monster"
+    def "checkPlayer1Dead should return true if player1 collides with a monster"() {
+        given: "a game with player1 and a monster"
         def player1 = Mock(Player)
-        def player2 = Mock(Player)
         def monster = Mock(Monster)
 
         level.getPlayer1() >> player1
-        level.getPlayer2() >> player2
         level.getMonsters() >> [monster]
         level.getTraps() >> []
 
@@ -32,27 +30,24 @@ class LevelControllerTest extends Specification {
         monster.getSizeX() >> 1
         monster.getSizeY() >> 1
         player1.hasCollided(monster.getPosition(), monster.getSizeX(), monster.getSizeY()) >> true
-        player2.hasCollided(_,_,_) >> false
 
         when: "the method is invoked"
-        def result = levelController.checkDeadPlayers()
+        def result = levelController.checkPlayer1Dead()
 
         then: "a collision is detected, and result is true"
         result
     }
 
-    def "checkDeadPlayers should return true if a player collides with a trap"() {
-        given: "a game with players and a trap"
+    def "checkPlayer1Dead should return true if player1 collides with a trap"() {
+        given: "a game with player1 and a trap"
         def player1 = Mock(Player)
-        def player2 = Mock(Player)
         def trap = Mock(Trap)
 
         level.getPlayer1() >> player1
-        level.getPlayer2() >> player2
         level.getTraps() >> [trap]
         level.getMonsters() >> []
 
-        and: "player1 collides with the monster"
+        and: "player1 collides with the trap"
         trap.getPosition() >> new Position (1,1)
         trap.getSizeX() >> 1
         trap.getSizeY() >> 1
@@ -60,10 +55,56 @@ class LevelControllerTest extends Specification {
         player1.getName() >> "Lavena"
         player1.hasCollided(trap.getPosition(), trap.getSizeX(), trap.getSizeY()) >> true
         player1.getName().equals(trap.getTarget()) >> true
-        player2.hasCollided(_,_,_) >> false
 
         when: "the method is invoked"
-        def result = levelController.checkDeadPlayers()
+        def result = levelController.checkPlayer1Dead();
+
+        then: "a collision is detected, and result is true"
+        result
+    }
+
+    def "checkPlayer2Dead should return true if player2 collides with a monster"() {
+        given: "a game with players and a monster"
+        def player2 = Mock(Player)
+        def monster = Mock(Monster)
+
+        level.getPlayer2() >> player2
+        level.getMonsters() >> [monster]
+        level.getTraps() >> []
+
+        and: "player2 collides with the monster"
+        monster.getPosition() >> new Position (1,1)
+        monster.getSizeX() >> 1
+        monster.getSizeY() >> 1
+        player2.hasCollided(monster.getPosition(), monster.getSizeX(), monster.getSizeY()) >> true
+
+        when: "the method is invoked"
+        def result = levelController.checkPlayer2Dead()
+
+        then: "a collision is detected, and result is true"
+        result
+    }
+
+    def "checkPlayer1Dead should return true if player2 collides with a trap"() {
+        given: "a game with a player 2 and a trap"
+        def player2 = Mock(Player)
+        def trap = Mock(Trap)
+
+        level.getPlayer2() >> player2
+        level.getTraps() >> [trap]
+        level.getMonsters() >> []
+
+        and: "player2 collides with the trap"
+        trap.getPosition() >> new Position (1,1)
+        trap.getSizeX() >> 1
+        trap.getSizeY() >> 1
+        trap.getTarget() >> "Lavena"
+        player2.getName() >> "Lavena"
+        player2.hasCollided(trap.getPosition(), trap.getSizeX(), trap.getSizeY()) >> true
+        player2.getName().equals(trap.getTarget()) >> true
+
+        when: "the method is invoked"
+        def result = levelController.checkPlayer2Dead();
 
         then: "a collision is detected, and result is true"
         result
