8a968455717e5bcce9541625f6534ef47c2d47b3
eitexd <goncalomfsantos@gmail.com>
2024-12-13 15:20:50 +0000

    fix: removed monster health

--- src/main/java/model/elements/movingelements/Monster.java
+++ src/main/java/model/elements/movingelements/Monster.java
@@ -5,18 +5,8 @@ import model.Position;
 import java.awt.image.BufferedImage;
 
 public class Monster extends MovingElement {
-    private int health;
 
-    public Monster(int health, Position position, BufferedImage image, int sizeX, int sizeY) {
+    public Monster(Position position, BufferedImage image, int sizeX, int sizeY) {
         super(position, image, sizeX, sizeY);
-        this.health = health;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
     }
 }
