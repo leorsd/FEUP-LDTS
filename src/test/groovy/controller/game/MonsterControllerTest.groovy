e73cdced39315b6f6049efc84f0236f71d898582
Leandro Resende <resendeleandro565@gmail.com>
2024-12-14 23:38:12 +0000

    fix: changed the tests due to the new movement of monsters

--- src/test/groovy/controller/game/MonsterControllerTest.groovy
+++ src/test/groovy/controller/game/MonsterControllerTest.groovy
@@ -23,41 +23,4 @@ class MonsterControllerTest extends Specification {
         then: "the monster position is updated"
         1 * monster.setPosition(freePosition)
     }
-
-    def "should not update monsters if time interval is too short"() {
-        given: "a MonsterController and Level with monsters"
-        def gameManager = Mock(GameManager)
-        def position = Mock(Position)
-
-        level.getMonsters() >> [monster]
-        monster.getPosition() >> position
-        position.getX() >> 5
-
-        monsterController.getLastUpdateTime() >> 1000
-
-        when: "update is called with insufficient time passed"
-        monsterController.update(gameManager, [] as Set, 1100)
-
-        then: "no monsters are moved"
-        0 * monster.setPosition(_)
-    }
-
-    def "should update monsters after sufficient time has passed"() {
-        given: "a MonsterController and Level with monsters"
-        def gameManager = Mock(GameManager)
-        def position = Mock(Position)
-
-        level.getMonsters() >> [monster]
-        monster.getPosition() >> position
-        position.getX() >> 10
-        position.getY() >> 10
-        level.isPositionFree(_ as Position) >> true
-        monsterController.getLastUpdateTime() >> 1000
-
-        when: "update is called with sufficient time passed"
-        monsterController.update((gameManager), [] as Set, 1300)
-
-        then: "monsters are moved to the desired position"
-        1 * monster.setPosition(_)
-    }
 }
