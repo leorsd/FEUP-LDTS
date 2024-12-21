c6c532abef10c12d009e62be62809a1c4e962c33
Leandro Resende <resendeleandro565@gmail.com>
2024-12-21 16:34:30 +0000

    fix: formatted the code for better readability

--- src/test/groovy/model/elements/dynamicelements/DoorTest.groovy
+++ src/test/groovy/model/elements/dynamicelements/DoorTest.groovy
@@ -6,34 +6,28 @@ import spock.lang.Specification
 class DoorTest extends Specification{
     def "Door should initialize with given position and size and default state CLOSED"() {
         given:
-        def position = new Position(10, 20)
-        def sizeX = 30
-        def sizeY = 40
-
+            def position = new Position(10, 20)
+            def sizeX = 30
+            def sizeY = 40
         when:
-        def door = new Door(position, sizeX, sizeY)
-
+            def door = new Door(position, sizeX, sizeY)
         then:
-        door.getPosition() == position
-        door.sizeX == sizeX
-        door.sizeY == sizeY
-        door.getState() == Door.STATE.CLOSED
+            door.getPosition() == position
+            door.sizeX == sizeX
+            door.sizeY == sizeY
+            door.getState() == Door.STATE.CLOSED
     }
 
     def "Door state should change when setState is called"() {
         given:
-        def door = new Door(new Position(0, 0), 10, 10)
-
+            def door = new Door(new Position(0, 0), 10, 10)
         when:
-        door.setState(Door.STATE.OPEN)
-
+            door.setState(Door.STATE.OPEN)
         then:
-        door.getState() == Door.STATE.OPEN
-
+            door.getState() == Door.STATE.OPEN
         when:
-        door.setState(Door.STATE.CLOSED)
-
+            door.setState(Door.STATE.CLOSED)
         then:
-        door.getState() == Door.STATE.CLOSED
+            door.getState() == Door.STATE.CLOSED
     }
 }
