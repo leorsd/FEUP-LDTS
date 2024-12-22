8d4f8a95522b02c298a2568ccf5f8c063c41e749
Leandro Resende <resendeleandro565@gmail.com>
2024-12-22 18:29:06 +0000

    feat: tested Scene visualizer class

---
+++ src/test/groovy/visualizer/SceneVisualizerTest.groovy
@@ -0,0 +1,40 @@
+package visualizer
+
+import gui.GUI
+import model.scenes.Menu
+import spock.lang.Specification
+
+class SceneVisualizerTest extends Specification {
+
+    class TestSceneVisualizer extends SceneVisualizer<Object> {
+        TestSceneVisualizer(Object scene) {
+            super(scene)
+        }
+
+        @Override
+        protected void drawElements(GUI gui) throws IOException {
+        }
+    }
+
+    def "test draw method calls correct GUI methods"() {
+        given:
+            def gui = Mock(GUI)
+            def scene = new Object()
+            def sceneVisualizer = new TestSceneVisualizer(scene)
+        when:
+            sceneVisualizer.draw(gui)
+        then:
+            1 * gui.clear()
+        and:
+            1 * gui.refresh()
+    }
+
+    def "test getScene returns correct scene"() {
+        given:
+            def scene = new Object()
+        when:
+            def sceneVisualizer = new TestSceneVisualizer(scene)
+        then:
+            sceneVisualizer.getScene() == scene
+    }
+}
