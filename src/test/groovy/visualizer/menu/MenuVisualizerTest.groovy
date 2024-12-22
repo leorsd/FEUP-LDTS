15b23be3229a821b2d256007fb742bbcaa5d86bb
Leandro Resende <resendeleandro565@gmail.com>
2024-12-22 18:28:14 +0000

    fix: make the test cover more code

--- src/test/groovy/visualizer/menu/MenuVisualizerTest.groovy
+++ src/test/groovy/visualizer/menu/MenuVisualizerTest.groovy
@@ -16,28 +16,22 @@ class MenuVisualizerTest extends Specification {
         when:
             menuVisualizer.drawElements(gui)
         then:
-            1 * gui.drawImage(_, _) // Ensure drawImage is called with the menu image
-            1 * gui.drawImage(_, _) // Ensure drawImage is called with the arrow image
-            6 * gui.drawImage(_, _) // Ensure drawImage is called 6 times for options
+            1 * gui.drawImage(_, _)
+            1 * gui.drawImage(_, _)
+            6 * gui.drawImage(_, _)
     }
 
     def "test drawElements when not enough entries in menu"() {
-        given: "Mocked GUI and Menu objects"
-        def gui = Mock(GUI)
-        def menu = Mock(Menu)
-
-        // Mock behavior of the menu
-        menu.getEntriesSize() >> 5 // 5 menu entries (less than 6, should trigger exception)
-        menu.getHighlightedEntryIndex() >> 0 // Highlighted entry at index 0
-
-        // Create the MenuVisualizer instance with the mocked menu
-        def menuVisualizer = new MenuVisualizer(menu)
-
-        when: "The drawElements method is called"
-        menuVisualizer.drawElements(gui)
-
-        then: "An exception is thrown due to insufficient entries"
-        def e = thrown(IOException)
-        e.message == "Not enough images for the entries of the menu"
+        given:
+            def gui = Mock(GUI)
+            def menu = Mock(Menu)
+            menu.getEntriesSize() >> 5
+            menu.getHighlightedEntryIndex() >> 0
+            def menuVisualizer = new MenuVisualizer(menu)
+        when:
+            menuVisualizer.drawElements(gui)
+        then:
+            def e = thrown(IOException)
+            e.message == "Not enough images for the entries of the menu"
     }
 }
